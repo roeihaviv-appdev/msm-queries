3d4d8fe4511005a90cb0fc02519249dc11cd643b
roeihaviv <rhaviv@chicagobooth.edu>
2022-04-30 22:55:41 +0000

    directors-youngest-eldest complete (1:09)

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -6,4 +6,16 @@ class DirectorsController < ApplicationController
     render({ :template => "director_templates/index.html.erb"})
   end
 
+  def wisest
+    
+    @oldest = Director.where.not({ :dob => nil }).order({ :dob => :asc }).at(0)
+    render({ :template => "director_templates/eldest.html.erb"})
+  end
+
+  def youngest
+
+    @youngest = Director.order({ :dob => :desc }).at(0)
+    render({ :template => "director_templates/youngest.html.erb"})
+  end
+
 end
